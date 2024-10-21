fef717e2160da8dd14a380e2b4d08b5a4f32c4a0
taritinth <tarit.khanbo@hotmail.com>
2024-10-21 14:09:29 +0700

    feat: add user data to firebase

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjXVFc6h-s1UJ5DyUxlmnF3moIG9K6aAc",
+  authDomain: "thainads-speed-dating.firebaseapp.com",
+  databaseURL:
+    "https://thainads-speed-dating-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "thainads-speed-dating",
+  storageBucket: "thainads-speed-dating.appspot.com",
+  messagingSenderId: "753163570473",
+  appId: "1:753163570473:web:132c525e1875d70f3a7b38",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { db, storage, auth };
--- main-v0.2.3.js
+++ main-v0.2.3.js
@@ -1,133 +1,16 @@
-// fetch(
-//   "https://discord.com/api/v9/users/281394328767692801/profile?guild_id=1036357772826120242",
-//   {
-//     headers: {
-//       accept: "*/*",
-//       "accept-language": "en-US,en;q=0.9",
-//       authorization:
-//         "MjgxMzk0MzI4NzY3NjkyODAx.G-h9z1.9SGgUjD8w02eGQL8PEY-NpDMQgATKy9OgQOaNg",
-//       priority: "u=1, i",
-//       "sec-ch-ua":
-//         '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
-//       "sec-ch-ua-mobile": "?0",
-//       "sec-ch-ua-platform": '"macOS"',
-//       "sec-fetch-dest": "empty",
-//       "sec-fetch-mode": "cors",
-//       "sec-fetch-site": "same-origin",
-//       "x-debug-options": "bugReporterEnabled",
-//       "x-discord-locale": "en-US",
-//       "x-discord-timezone": "Asia/Bangkok",
-//       "x-super-properties":
-//         "eyJvcyI6Ik1hYyBPUyBYIiwiYnJvd3NlciI6IkNocm9tZSIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJlbi1VUyIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChNYWNpbnRvc2g7IEludGVsIE1hYyBPUyBYIDEwXzE1XzcpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8xMjkuMC4wLjAgU2FmYXJpLzUzNy4zNiIsImJyb3dzZXJfdmVyc2lvbiI6IjEyOS4wLjAuMCIsIm9zX3ZlcnNpb24iOiIxMC4xNS43IiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjMyOTgwNCwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0=",
-//       cookie:
-//         "__dcfduid=20b4d2a0798411ef92174b63dff40ea4; __sdcfduid=20b4d2a1798411ef92174b63dff40ea42e49d512fe72f133aab26589a55dfc993263bd9512b5efbd22c560bbf18d164b; locale=en-US; _gcl_au=1.1.1888767483.1727079387; _ga_Q149DFWHT7=GS1.1.1727079387.1.1.1727079401.0.0.0; _gid=GA1.2.349316054.1727082018; __cfruid=1eab8d80a0003e49b9bdd70ae2278351f57d93dd-1727144544; _cfuvid=4udeWRsh_mGz5jXaZTE7mB_gZ7A_xTo0Zs3TbCDA.9I-1727144544273-0.0.1.1-604800000; cf_clearance=CTtAXBID5cKQnfYxnHVsdCg2z3WioFcGVax8eVNDBmk-1727150530-1.2.1.1-I4Tw0XEUV9wZE9oNJ_kI45..F6anuF_7wSZPTjJ5R8Qi5zE6sWST0bWXJ5Z7Dj_3.jywbaaXbjgG_4TirTWjkaLEwRDxj5hj_EICPvM1dVr5jjT9cPMffIvK2RjwmLWgcySFgNExX231wh_HoH0FV_CrkcElMrVlAQ6Awj7ps92QgPGE2NbcLOOqFPhTJ2MXUvsEGx8hqrEzdL4SNiaMR1x.O.jm28D4J7GGqVtrlwqftUSp0.rhRgvgi51Gzw9Sz1zLEYPT1Nli.WhYxcIBTQLNIpkdmg7GWbkEs0mLr0oL2Mst8EcvVjko4B.c_7gyb8pcTgEfpajZ.lP1X7aGeVGecbv9RuCR_FNVeywVftxsoo7YKJwwdxVjP8EqxUPBZZecnHy3Tdv_F9UzwJM5PA; _ga=GA1.1.2031107531.1727079387; OptanonConsent=isIABGlobal=false&datestamp=Tue+Sep+24+2024+11%3A03%3A02+GMT%2B0700+(Indochina+Time)&version=6.33.0&hosts=&landingPath=https%3A%2F%2Fdiscord.com%2F&groups=C0001%3A1%2CC0002%3A1%2CC0003%3A1; _ga_YL03HBJY7E=GS1.1.1727150582.2.0.1727150587.0.0.0",
-//       Referer:
-//         "https://discord.com/channels/1036357772826120242/1159504868692742166/1287649943439540287",
-//       "Referrer-Policy": "strict-origin-when-cross-origin",
-//     },
-//     body: null,
-//     method: "GET",
-//   }
-// );
-
-// const puppeteer = require("puppeteer-core");
-// const clipboard = require("clipboardy");
-
 import puppeteer from "puppeteer-core";
 import fs from "fs";
 
-// const usernamesToSearch = [
-//   "billmonday",
-//   "jackbenjamin",
-//   "boba5000",
-//   "kan2106",
-//   "frank_168",
-//   "ik3377",
-//   "lilmatic",
-//   "yap00012",
-//   "lkyd01",
-//   "nongwaan",
-//   "bundo9",
-//   "0xneaf",
-//   "kenobi_eth",
-//   "noey168",
-//   "hefs_2200",
-//   "jonathanhnd11",
-//   "tmdv8",
-//   "reborn8958",
-//   "dos4289",
-//   "davenguyencrp",
-//   "onlinelink",
-//   "lastexamdoteth",
-//   "siomay.eth",
-// ]; // Array of usernames to search
+import { encodeUsername } from "./utils.js";
+import { ref, set, get } from "firebase/database";
+import { db, storage } from "./firebase.js";
 
-// const usernamesToSearch = [
-//   "be_kindplss",
-//   "kan2106",
-//   "jackbenjamin",
-//   "chackky",
-//   "apesage",
-//   "papayou",
-//   "ceeedweak",
-//   "jedlyk",
-//   "dani.dan",
-//   "n1c0cs",
-//   "matti003",
-//   "baikan",
-//   "erkincem",
-//   "cusandara",
-//   "cryptoaga",
-//   "sugar6378",
-//   "minunplusle",
-//   "jackhauteville",
-//   "billmonday",
-//   "jackbenjamin",
-//   "boba5000",
-//   "0xh3x",
-//   "kan2106",
-//   "frank_168",
-//   "ik3377",
-//   "lilmatic",
-//   "yap00012",
-//   "lkyd01",
-//   "nongwaan",
-//   "bundo9",
-//   "0xneaf",
-//   "kenobi_eth",
-//   "noey168",
-//   "hefs_2200",
-//   "jonathanhnd11",
-//   "tmdv8",
-//   "reborn8958",
-//   "dos4289",
-//   "davenguyencrp",
-//   "onlinelink",
-//   "lastexamdoteth",
-//   "siomay.eth",
-//   "lydicius",
-//   "shubhx",
-//   "yhl",
-//   "heikel",
-//   "ristrettocat",
-//   "dooonut",
-//   "willy4619",
-//   "oldsix",
-//   "bigcat6680",
-//   "deneth",
-//   "jonski7768",
-//   "hong9292",
-//   "gurt5914",
-//   "arf11",
-//   "firehaux",
-//   "tolkien41",
-//   "cal1.f",
-//   "ssick",
-//   "shimmoney",
-//   "cryptobulz",
-//   "totemforyou",
-//   "brocoliwang",
-// ]; // Array of usernames to search
+import {
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
+// import { v4 as uuidv4 } from "uuid";
 
 const discordIdsLowerCase = [
   "vodkla",
@@ -159,7 +42,6 @@ const discordIdsLowerCase = [
   "stargirl.007",
   "jasonreal",
   "cryptoaga",
-  "trenny gf",
   "jen_nyt0314",
   "mthuy0301",
   "leonardo_real",
@@ -212,7 +94,7 @@ const discordIdsLowerCase = [
   "veroxis",
   "donut0369",
   "kraius.",
-  "port_dev",
+  "portdev",
   "nathan8644",
   "frogwell",
   "eviltahaaa",
@@ -223,7 +105,7 @@ const discordIdsLowerCase = [
   "mindymolly124",
   "zella.nft",
   "arcyeth",
-  "brocoliwangg",
+  "brocoliwang",
   ".geekyoptimist",
   "shroomskull",
   "0xmichu",
@@ -247,7 +129,6 @@ const discordIdsLowerCase = [
   "aedan5146",
   "mr.mnm.",
   "yung2105",
-  "zeuz777",
   "0xreinn",
   "tequilawaifu",
   "_yeeet",
@@ -259,7 +140,6 @@ const discordIdsLowerCase = [
   "oak_._._",
   "kaybuuuuuu",
   "wani19kk",
-  "milawh",
   "puresoul0109",
   "jasonreal",
   "sharkdogsad",
@@ -304,7 +184,7 @@ const discordIdsLowerCase = [
 
       // Wait for the search results to appear
       await page.waitForSelector('[role^="option"]', {
-        timeout: 10000,
+        timeout: 5000,
       });
 
       // Extract all results
@@ -371,14 +251,52 @@ const discordIdsLowerCase = [
         avatarUrl.match(/\/avatars\/(\d+)\//);
       const userId = userIdMatch ? userIdMatch[1] : "User ID not found";
 
+      // Reference to the "users" collection in the Realtime Database
+      const usersRef = ref(db, `users/${encodeUsername(username)}`);
+
+      const userSnapshot = await get(usersRef);
+
+      if (userSnapshot.exists()) {
+        console.log(`User ${username} already exists.`);
+        return;
+      }
+
+      // Generate a unique filename using uuid for the avatar
+      const uniqueFileName = `${encodeUsername(username)}.webp`;
+
+      // Upload the avatar image to Firebase Storage
+      const avatarStorageRef = storageRef(storage, `avatars/${uniqueFileName}`);
+      const avatarResponse = await fetch(avatarUrl); // Fetch the image from the original URL
+      const avatarBlob = await avatarResponse.blob(); // Convert the image to a blob
+
+      // Upload avatar blob to Firebase Storage with a UUID filename
+      await uploadBytes(avatarStorageRef, avatarBlob, {
+        cacheControl: "public,max-age=31536000", // 1 year cache
+      });
+
+      // Get the download URL for the uploaded avatar
+      const avatarDownloadUrl = await getDownloadURL(avatarStorageRef);
+
+      // Add user data (with avatarDownloadUrl) to the Realtime Database
+      await set(usersRef, {
+        userId: userId,
+        username: username,
+        displayName: displayName,
+        avatarUrl: avatarDownloadUrl, // Save the download URL of the avatar
+        connections: {}, // Initially empty connections
+        lastActive: Date.now(), // Unix timestamp for last active time
+      });
+
+      console.log(`User ${username} added successfully with avatar.`);
+
       const data = {
         userId,
         displayName,
         username,
-        avatarUrl,
+        avatarUrl: avatarDownloadUrl,
       };
 
-      console.log(data);
+      // console.log(data);
       resultsData.push(data);
 
       totalSuccess += 1;
